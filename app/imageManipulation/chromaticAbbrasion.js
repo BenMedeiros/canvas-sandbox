8f8639769d658bfb59a3f60ab501e9fcfb352de6
Ben <ben.medeiros@planttherapy.com>
2023-10-03 21:27:00 -0600

    todo add chromatic abbrasion

---
+++ app/imageManipulation/chromaticAbbrasion.js
@@ -0,0 +1,8 @@
+'use strict';
+//TODO
+/* update image with chromatic abbrasion, which spreads colors out from center
+so hypt(x,y) drives shift of color away, red shifts least, purple shifts most.
+* */
+export default function main(){
+
+}
